df31d25496330ecbe9e935817ecb18fd08627408
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2025-02-16 19:39:04 -0500

    feat: add create user logic

---
+++ functions/src/feautres/user/application/CreateUserUseCase.ts
@@ -0,0 +1,30 @@
+import { IUserRepository } from "../domain/IUserRepository";
+import { UserFactory } from "../domain/UserFactory";
+import { User } from "../domain/User";
+import { CustomError } from "../../../shared/errors/CustomError";
+
+interface CreateUserDTO {
+  email: string;
+  name: string;
+  lastName: string;
+}
+
+export class CreateUserUseCase {
+  constructor(private readonly userRepository: IUserRepository) {}
+
+  public async execute(dto: CreateUserDTO): Promise<User> {
+    const existing = await this.userRepository.findByEmail(dto.email);
+    if (existing) {
+      throw new CustomError(`El usuario con email ${dto.email} ya existe`, 400);
+    }
+
+    const user = UserFactory.create({
+      email: dto.email,
+      name: dto.email,
+      lastName: dto.lastName,
+    });
+
+    const createdUser = await this.userRepository.create(user);
+    return createdUser;
+  }
+}
